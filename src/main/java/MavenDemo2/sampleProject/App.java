22f2730ac51438b3440668164225879ec81e0168
Vaishnavi <vaishna2428@gmail.com>
2026-08-19 12:33:39 +0530

    initial commit

---
+++ src/main/java/MavenDemo2/sampleProject/App.java
@@ -0,0 +1,13 @@
+package MavenDemo2.sampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
